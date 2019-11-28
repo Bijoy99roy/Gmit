7ddc08eaa5d7e75c33c95263d29823b1c4b3b2c2
Bijoy99roy <51710621+Bijoy99roy@users.noreply.github.com>
2019-11-28 07:55:33 +0530

    Update Knapsack.c

--- Knapsack.c
+++ Knapsack.c
@@ -1,142 +1,91 @@
-#include<stdio.h>
-void merge(float arr[],int l,int m,int r)
-{
-    int i,j,k;
-    int n1=m-l+1;
-    int n2=r-m;
-    float L[n1],R[n2];
-    for(i=0;i<n1;i++)
-        L[i]=arr[l+i];
-    for(j=0;j<n2;j++)
-        R[j]=arr[m+1+j];
-    i=0;
-    j=0;
-    k=l;
-    while(i<n1&&j<n2)
-    {
-        if(L[i]<=R[j])
-        {
-            arr[k]=L[i];
-            i++;
-        }
-        else
-        {
-            arr[k]=R[j];
-            j++;
-        }
-        k++;
-    }
-    while(i<n1)
-    {
-        arr[k]=L[i];
-        i++;
-        k++;
-    }
-   while(j<n2)
-    {
-        arr[k]=R[j];
-        j++;
-        k++;
-    }
+# include<stdio.h>
+
+void knapsack(int n, float weight[], float profit[], float capacity) {
+   float x[20], tp = 0;
+   int i, j, u;
+   u = capacity;
+
+   for (i = 0; i < n; i++)
+      x[i] = 0.0;
+
+   for (i = 0; i < n; i++) {
+      if (weight[i] > u)
+         break;
+      else {
+         x[i] = 1.0;
+         tp = tp + profit[i];
+         u = u - weight[i];
+      }
+   }
+
+   if (i < n)
+      x[i] = u / weight[i];
+
+   tp = tp + (x[i] * profit[i]);
+
+   printf("\nThe result vector is:- ");
+   for (i = 0; i < n; i++)
+      printf("%f\t", x[i]);
+
+   printf("\nMaximum profit is:- %f", tp);
+
 }
 
-void mergeSort(float arr[],int l,int r)
-{
-    if(l<r)
-    {
-        int m=l+(r-l)/2;
-        mergeSort(arr,l,m);
-        mergeSort(arr,m+1,r);
-        merge(arr,l,m,r);
-    }
+int main() {
+   float weight[20], profit[20], capacity;
+   int num, i, j;
+   float ratio[20], temp;
+
+   printf("\nEnter the no. of objects:- ");
+   scanf("%d", &num);
+
+   printf("\nEnter the wts and profits of each object:- ");
+   for (i = 0; i < num; i++) {
+      scanf("%f %f", &weight[i], &profit[i]);
+   }
+
+   printf("\nEnter the capacityacity of knapsack:- ");
+   scanf("%f", &capacity);
+
+   for (i = 0; i < num; i++) {
+      ratio[i] = profit[i] / weight[i];
+   }
+
+   for (i = 0; i < num; i++) {
+      for (j = i + 1; j < num; j++) {
+         if (ratio[i] < ratio[j]) {
+            temp = ratio[j];
+            ratio[j] = ratio[i];
+            ratio[i] = temp;
+
+            temp = weight[j];
+            weight[j] = weight[i];
+            weight[i] = temp;
+
+            temp = profit[j];
+            profit[j] = profit[i];
+            profit[i] = temp;
+         }
+      }
+   }
+
+   knapsack(num, weight, profit, capacity);
+   return(0);
 }
-int main()
+
 {
-    int numberOfObjects,i,j,n;
-    float pw[20],x[20],arr[20],profit[20],weight[20],sackValue,xiwi = 0;
+    Enter the no. of objects:- 7
 
-    printf("Number of objects:");
-    scanf("%d",&n);
+Enter the wts and profits of each object:- 2 10
+3 5
+5 15
+7 7
+1 6
+4 18
+1 3
 
-    printf("Enter sackValue: ");
-    scanf("%f",&sackValue);
+Enter the capacityacity of knapsack:- 15
 
-    printf("Enter profit and weight:\n");
-    printf("profit\tweight\n");
-    for(i=0;i<n;i++)
-    {
-        scanf("%f%f",&profit[i],&weight[i]);
-
-    }
-    printf("P/W ratio:\n");
-    for(i=0;i<n;i++)
-    {
-        pw[i]=profit[i]/weight[i];
-
-    }
-
-    for(i=0;i<n;i++)
-    {
-        arr[i]=pw[i];
-        printf("%f   ",pw[i]);
-    }
-
-
-    mergeSort(arr,0,n-1);
-
-    for(i=0;i<n;i++)
-    {
-
-        printf("\n%f ",arr[i]);
-    }
-
-    for(i=0;i<n;i++)
-    {
-
-        printf("\nw:%f ",weight[i]);
-    }
-
-    float val;
-    int p=0;
-    for(i=n-1;i>=0;i--)
-    {
-            val=arr[i];
-            for(j=0;j<n;j++)
-            {
-                if(val==pw[j])
-                {
-                if(weight[j]<sackValue)
-                {
-                        p++;
-                        sackValue-=weight[j];
-                        x[j]=1;
-                    }
-                    else if(weight[j]>sackValue)
-                    {
-                        p++;
-                        x[j]=sackValue/weight[j];
-                        sackValue=0;
-                    }
-                    else if(sackValue==0)
-                    {
-                        p++;
-                        x[j]=0;
-                    }
-                    break;
-                }
-            }
-    }
-    for(i=0;i<n;i++)
-    {
-        printf("\nx:%f",x[i]);
-    }
-
-    printf("\nValue of xiwi:");
-    for(i=0;i<n;i++)
-    {
-     xiwi=xiwi+(x[i]*weight[i]);
-    }
-    printf("%f",xiwi);
-    printf("\n%d",p);
-
+The result vector is:- 1.000000 1.000000        1.000000        1.000000        1.000000        0.666667        0.000000
+Maximum profit is:- 55.333332
 }
